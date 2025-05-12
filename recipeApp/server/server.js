be238dfb392ba2e894d103586205249631c40df9
Hyomin <hyomin.kim@stonybrook.edu>
2025-05-12 17:23:53 +0900

    update rejectUnauthorized

--- recipeApp/server/server.js
+++ recipeApp/server/server.js
@@ -25,6 +25,9 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 console.log("Success to connect");
