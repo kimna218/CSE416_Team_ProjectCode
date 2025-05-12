b9f93807283b677477ce53f338908ce8bc74b128
Hyomin <hyomin.kim@stonybrook.edu>
2025-05-12 18:28:52 +0900

    update server.js

--- recipeApp/server/server.js
+++ recipeApp/server/server.js
@@ -11,6 +11,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.resolve(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
+});
+
+
 console.log("📡 Connecting to DB...");
 console.log({
   host: process.env.DB_HOST,
@@ -178,15 +191,4 @@ app.listen(PORT, async () => {
   } catch (err) {
     console.error("Cannot start server:", err);
   }
-});
-
-import path from "path";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.resolve(__dirname, "../dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
 });
