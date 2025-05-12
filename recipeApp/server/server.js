5b6aa66fd108644f662885bc1d1e5154aa7dc827
Hyomin <hyomin.kim@stonybrook.edu>
2025-05-12 18:18:49 +0900

    update server.js

--- recipeApp/server/server.js
+++ recipeApp/server/server.js
@@ -185,8 +185,8 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, "../dist")));
+app.use(express.static(path.resolve(__dirname, "../dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "../dist", "index.html"));
 });
