58f6b589831693768b7bb3cf5bed346ee62407e5
Hyomin <hyomin.kim@stonybrook.edu>
2025-05-13 09:45:12 +0900

    ✅ Railway PostgreSQL 연동

--- recipeApp/server/server.js
+++ recipeApp/server/server.js
@@ -21,18 +21,9 @@ console.log({
 });
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
-  },
-
-  connectionTimeoutMillis: 5000,
-  lookup: (hostname, options, callback) => {
-    require("dns").lookup(hostname, { family: 4 }, callback);
   }
 });
 
