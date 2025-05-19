5324ece4ee3321f460ae7b49878278ef39ace083
Hyomin <hyomin.kim@stonybrook.edu>
2025-05-19 16:01:56 +0900

    remove unncessary statement

--- recipeApp/src/pages/Home.tsx
+++ recipeApp/src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../css/Home.css";
-import { getAuth } from "firebase/auth";
 
 interface Recipe {
   id: number;
