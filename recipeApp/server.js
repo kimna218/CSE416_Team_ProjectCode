a44bb6408611e10dcbad11308d1a5c515814352e
Hyomin <hyomin.kim@stonybrook.edu>
2025-05-19 16:20:13 +0900

    update using gpt3.5

--- recipeApp/server.js
+++ recipeApp/server.js
@@ -745,7 +745,7 @@ ${recipeListString}
 `;
 
     const response = await openai.chat.completions.create({
-      model: "gpt-4",
+      model: "gpt-3.5-turbo",
       messages: [
         {
           role: "user",
